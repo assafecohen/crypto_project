859c1cf07f41301351b2f75adfea22189097e441
Assafm <assafe.cohen@gmail.com>
2020-02-11 02:58:17 +0200

    Added AppBar

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import styled, { css } from 'styled-components';
+
 import WelcomeMessage from './WelcomeMessage';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 function App() {
   return (
     <div className='App'>
       <AppLayout>
+        <AppBar />
         <WelcomeMessage />
       </AppLayout>
     </div>
