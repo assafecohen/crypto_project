9d08105d8ffea6eaa98473e8882097323f0837c9
Assafm <assafe.cohen@gmail.com>
2020-02-11 02:42:44 +0200

    added styled-components

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import styled, { css } from 'styled-components';
 
 function App() {
-  return <div className='App'>hello world</div>;
+  return (
+    <div className='App'>
+      <WelcomeMessage />
+    </div>
+  );
 }
 
 export default App;
