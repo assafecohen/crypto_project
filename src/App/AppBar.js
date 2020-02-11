0999a8aee66b95c6abe4cb4f25b918716a331b04
Assafm <assafe.cohen@gmail.com>
2020-02-11 03:13:27 +0200

    Styled AppBar

---
+++ src/App/AppBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
+
+const Bar = styled.div`
+  display: grid;
+  margin-bottom: 40px;
+  grid-template-columns: 180px auto 100px 100px;
+`;
+
+const ControlButtonElm = styled.div`
+  cursor: pointer;
+  ${props =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 60px #03ff03;
+    `}
+`;
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+function ControlButton({ name, active }) {
+  return (
+    <ControlButtonElm active={active}>{toProperCase(name)}</ControlButtonElm>
+  );
+}
+
+const AppBar = () => {
+  return (
+    <Bar>
+      <Logo> CryptoDash</Logo>
+      <div></div>
+      <ControlButton active name='dashboard' />
+      <ControlButton name='setting' />
+    </Bar>
+  );
+};
+
+export default AppBar;
