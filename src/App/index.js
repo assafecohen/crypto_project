26faeed323774b0006d381ab8afb041aa8912564
Assafm <assafe.cohen@gmail.com>
2020-02-29 23:43:53 +0200

    change files arch

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './App.css';
 import styled, { css } from 'styled-components';
-
-import WelcomeMessage from './WelcomeMessage';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
 import AppProvider from './AppProvider';
+import Settings from '../Settings';
 function App() {
   return (
     <div className='App'>
       <AppLayout>
         <AppProvider>
           <AppBar />
-          <WelcomeMessage />
+          <Settings />
         </AppProvider>
       </AppLayout>
     </div>
